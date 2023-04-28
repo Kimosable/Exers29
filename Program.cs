4a33adb15bc7578a67d7cea25b8f6374e66078eb
Kimosable <129333154+Kimosable@users.noreply.github.com>
2023-04-28 14:38:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+System.Console.WriteLine("Введите число ");
+int number = int.Parse(System.Console.ReadLine());
+string number1 = number.ToString();
+int len = number1.Length;
+
+int [] array = new int [len];
+for(int i = 0; i < len; i++)
+{
+    array[i] = int.Parse(number1[i].ToString());
+}
+
+for(int i = 0; i < len; i++ )
+{
+    System.Console.Write(array[i]);
+}
